5457a2c88fa7f345a99b8af0c3924e83d2bf6d0c
Sov3rain <samuel.k.duval@gmail.com>
2021-11-23 16:40:04 +0100

    cleanup simple example, move message types in their own file

--- Assets/Messager/Samples/Demos/Scripts/SimpleExample.cs
+++ Assets/Messager/Samples/Demos/Scripts/SimpleExample.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class SimpleExample : MonoBehaviour
 {
-    readonly private Messager _messager = Messager.DefaultInstance;
+    readonly Messager _messager = Messager.DefaultInstance;
 
     void Start()
     {
         _messager
             .Listen<SIMPLE_MESSAGE>(
                 owner: this,
-                (msg) => Debug.Log("Anonymous handler!")
+                handler: msg => Debug.Log("Anonymous handler!")
             )
             .Listen<OTHER_MESSAGE>(
                 owner: this,
-                (msg) => Debug.Log($"Count: {msg.count}")
+                handler: msg => Debug.Log($"Count: {msg.count}")
             );
     }
 
     [ContextMenu("Fire Event")]
     void FireEvent()
     {
-        _messager.Dispatch(new SIMPLE_MESSAGE { count = 100 });
+        _messager.Dispatch(new SIMPLE_MESSAGE { Count = 100 });
         _messager.Dispatch(new OTHER_MESSAGE(count: 100));
     }
 
@@ -28,20 +28,5 @@ public class SimpleExample : MonoBehaviour
     void Cut()
     {
         _messager.Cut<SIMPLE_MESSAGE>(this);
-    }
-}
-
-public sealed class SIMPLE_MESSAGE
-{
-    public int count;
-}
-
-readonly public struct OTHER_MESSAGE
-{
-    readonly public int count;
-
-    public OTHER_MESSAGE(int count)
-    {
-        this.count = count;
     }
 }
